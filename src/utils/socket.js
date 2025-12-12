b4d9beefb1bc38cd198458f2dc10851b2fdc7869
Kirtan Kumar <kirtankumar9310136575@gmail.com>
2025-12-12 13:48:54 +0530

    Change socket URL to production endpoint

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,11 +1,12 @@
 import { io } from "socket.io-client";
 import conf from "../conf/conf";
 
-const SOCKET_URL = "http://localhost:4000";
+const SOCKET_URL = "https://opticalbackend-25yf.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
   auth: {
     token: localStorage.getItem("accessToken"),
   },
+
 });
